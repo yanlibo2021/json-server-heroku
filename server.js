80c86df3f842e89b6beb520ed4a88332ee0e1cae
yanlibo2021 <yanlibo2021@126.com>
2020-03-21 22:31:15 +0800

    new file:   .gitignore         new file:   README.md         new file:   db.json         new file:   package.json         new file:   server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+// server.use(router);
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    "/del/user/:id": "/userlist/:id",
+    "/modify/user/:id": "/userlist/:id",
+    "/add/user": "/userlist",
+    "/del/flow/:id": "/flowlist/:id",
+    "/modify/flow/:id": "/flowlist/:id",
+    "/add/flow": "/flowlist",
+    "/rtc/steps/settings/source": "/source",
+    "/rtc/steps/settings/sink": "/sink"
+}))
+server.use("/api", router);
+
+server.listen(port);
+
+
+
+
+//test
+
+
+
+
